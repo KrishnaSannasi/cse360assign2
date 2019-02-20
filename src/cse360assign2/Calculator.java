73ff6f9366df6f89d14cca095597499e6dccad1e
Ozaren <krishna.sd.2012@gmail.com>
2019-02-19 19:37:17 -0700

    Added documentation

---
+++ src/cse360assign2/Calculator.java
@@ -0,0 +1,72 @@
+
+package cse360assign2;
+
+public class Calculator {
+    /**
+     * An accumulator for use in all of the calculator methods
+     */
+    private int total;
+    
+    /**
+     * Makes a new calculator with an initial value of 0
+     */
+    public Calculator () {
+        total = 0;  // not needed - included for clarity
+    }
+    
+    /**
+     * Gets the current <code>total</code> value
+     * 
+     * @return <code>total</code> value that has accumulated
+     */
+    public int getTotal () {
+        return 0;
+    }
+    
+    /**
+     * Adds <code>value</code> to the current <code>total</code>
+     * 
+     * @param <code>value</code>
+     */
+    public void add (int value) {
+        
+    }
+    
+    /**
+     * Subtracts <code>value</code> from the current <code>total</code>
+     * 
+     * @param <code>value</code>
+     */
+    public void subtract (int value) {
+        
+    }
+    
+    /**
+     * Multiplies <code>value</code> to the current <code>total</code>
+     * 
+     * @param <code>value</code>
+     */
+    public void multiply (int value) {
+        
+    }
+    
+    /**
+     * Divides <code>total</code> by <code>value</code>
+     * 
+     * note: if <code>value</code> is 0, then <code>total</code> is set to 0
+     * 
+     * @param <code>value</code>
+     */
+    public void divide (int value) {
+        
+    }
+    
+    /**
+     * Gets the current history of actions
+     * 
+     * @return history
+     */
+    public String getHistory () {
+        return "";
+    }
+}
